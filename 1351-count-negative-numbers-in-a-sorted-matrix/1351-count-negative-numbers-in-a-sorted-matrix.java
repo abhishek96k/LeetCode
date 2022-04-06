6988bff6d0294e2b627e903f45ce53e0222519b1
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-04-06 23:22:53 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (95.66%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,27 +1,24 @@
 class Solution {
     public int countNegatives(int[][] grid) {
-        int res = 0;
-        for(int i = 0; i < grid.length; ++i) {
-            // add up negative numbers of each row
-            res += negativeEachRow(grid[i]);
+    int res = 0;
+    for(int i = 0; i < grid.length; ++i) { 
+        res += negativeEachRow(grid[i]);
+    }
+    return res;
+}
+
+private int negativeEachRow(int[] row){
+
+    int res = 0, low = 0, high = row.length - 1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(row[mid] >= 0){
+            low = mid + 1;
+        } else if (row[mid] < 0){
+            res += high - mid + 1;
+            high = mid - 1;
         }
+       }
         return res;
     }
-    
-    private int negativeEachRow(int[] row) {
-        int res = 0;
-        int l = 0, r = row.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            // if midPoint is positive, go to the right side
-            // if midPoint is negative, count the right side(because they are all negative) and go to left side.
-            if (row[mid] >= 0) {
-                l = mid + 1;
-            } else if (row[mid] < 0) {
-                res += r - mid + 1;
-                r = mid - 1;
-            }
-        }
-        return res;
-    }
-    }
+}
