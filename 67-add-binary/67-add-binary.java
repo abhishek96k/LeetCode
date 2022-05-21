c9707712a6a14da46178d6ad3db76beab444cf2a
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-05-21 11:39:59 +0530

    Time: 2 ms (83.08%), Space: 42.5 MB (65.24%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,46 +1,19 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int carry = 0;
-        
-        StringBuilder s1 = new StringBuilder(a);
-        StringBuilder s2 = new StringBuilder(b);
         StringBuilder res = new StringBuilder();
-        
-        s1.reverse();
-        s2.reverse();
-        
-        int i = 0, sum;
-        
-        while(i < a.length() || i < b.length()){
-            sum = carry;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
             
-            if(i < a.length()) sum += s1.charAt(i) - '0';
-            if(i < b.length()) sum += s2.charAt(i) - '0';
-            
-            if(sum == 0)
-            {
-                carry = 0;
-                res.append('0');
-            }
-            if(sum == 1)
-            {
-                carry = 0;
-                res.append('1');
-            }
-            if(sum == 2)
-            {
-                carry = 1;
-                res.append('0');
-            }
-            if(sum == 3)
-            {
-                carry = 1;
-                res.append('1');
-            }
-            i++;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
         }
-        if(carry == 1) res.append('1');
-        res.reverse();
-        return res.toString();
+        if(carry != 0)
+            res.append(carry);
+        return res.reverse().toString();
     }
 }
