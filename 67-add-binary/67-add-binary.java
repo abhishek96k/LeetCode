8d95a9c317c13041e51b60e61b9eda7d7b033748
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-05-24 09:35:42 +0530

    Time: 2 ms (83.10%), Space: 42.4 MB (69.38%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -4,16 +4,17 @@ class Solution {
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
-            
+        
         while(i >= 0 || j >= 0){
             int sum = carry;
-            if(i >= 0) sum += a.charAt(i--) - '0';
-            if(j >= 0) sum += b.charAt(j--) - '0';
+            
+            sum += (i >= 0) ? a.charAt(i--) - '0': 0;
+            sum += (j >= 0) ? b.charAt(j--) - '0': 0;
+            
             carry = sum > 1 ? 1 : 0;
             res.append(sum % 2);
         }
-        if(carry != 0)
-            res.append(carry);
+        if(carry != 0) res.append(carry);
         return res.reverse().toString();
     }
 }
