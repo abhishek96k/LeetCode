91906c03594b8589ea1da9dbf87937c4451694d6
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-05-30 09:52:05 +0530

    Time: 1 ms (99.25%), Space: 41.7 MB (28.56%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,38 +1,20 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-    boolean isNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? true : false;
+        boolean isNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)? true : false;
         
-    long absDividend = Math.abs((long) dividend);
-    long absDivisor = Math.abs((long) divisor);
-    long result = 0;
+        long absDivident = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+        long result = 0;
         
-    while(absDividend >= absDivisor){
-        long tmp = absDivisor, count = 1;
-        while(tmp <= absDividend){
-            tmp <<= 1;
-            count <<= 1;
+        while(absDivident >= absDivisor){
+            long temp = absDivisor, count = 1;
+            while(temp <= absDivident){
+                temp <<= 1;
+                count <<= 1;
+            }
+            result += count >> 1;
+            absDivident -= temp >> 1;
         }
-        result += count >> 1;
-        absDividend -= tmp >> 1;
+        return isNegative ? (int) ~result + 1 : result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
     }
-    return  isNegative ? (int) ~result + 1 : result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
 }
-}
-
-/*
-boolean isNegative = (divided < 0 && divisor > 0) || (dividend > 0 && divisor < 0)? true : false;
-long absDividend = Math.abs((long) dividend);
-long absDivisor = Math.abs((long) divisor);
-long result = 0;
-    while(absDividend >= absDivisor){
-    long temp = absDivisor, count = 1;
-    while(temp <= absDividend){
-    temp <<= 1;
-    count <<= 1;
-    }
-    result += count >> 1;
-    absDividend -= temp >> 1;
-    }
-    return isNegative ? (int) ~result + 1: result > Integer.MAX_VALUE? Integer.MA
-
-*/
