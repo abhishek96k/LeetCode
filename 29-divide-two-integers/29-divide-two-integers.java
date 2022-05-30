b0c86a1f9bd663e49aa0ea8e9bb1e9144c2cfef1
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-05-30 09:40:21 +0530

    Time: 3 ms (24.06%), Space: 41.4 MB (52.74%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -18,3 +18,21 @@ class Solution {
     return  isNegative ? (int) ~result + 1 : result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
 }
 }
+
+/*
+boolean isNegative = (divided < 0 && divisor > 0) || (dividend > 0 && divisor < 0)? true : false;
+long absDividend = Math.abs((long) dividend);
+long absDivisor = Math.abs((long) divisor);
+long result = 0;
+    while(absDividend >= absDivisor){
+    long temp = absDivisor, count = 1;
+    while(temp <= absDividend){
+    temp <<= 1;
+    count <<= 1;
+    }
+    result += count >> 1;
+    absDividend -= temp >> 1;
+    }
+    return isNegative ? (int) ~result + 1: result > Integer.MAX_VALUE? Integer.MA
+
+*/
