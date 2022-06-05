f4ac418f3a55748e2dcf4b791526f880bf4b91e7
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-06-05 23:14:10 +0530

    Time: 39 ms (39.21%), Space: 64 MB (37.64%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -1,16 +1,12 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         
-        Set<String> set = new HashSet<>();
-        Set<String> list = new HashSet<>();
-        for(int i = 0; i <= s.length() - 10; i++){
-            String substring = s.substring(i, i + 10);
-            if(set.contains(substring)) {
-                list.add(substring);
-            } else {
-                set.add(substring);
-            }
-        }
-        return new ArrayList(list);
+        Set seen = new HashSet(), repeated = new HashSet();
+    for (int i = 0; i + 9 < s.length(); i++) {
+        String ten = s.substring(i, i + 10);
+        if (!seen.add(ten))
+            repeated.add(ten);
+    }
+    return new ArrayList(repeated);
     }
 }
