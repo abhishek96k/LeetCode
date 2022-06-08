0b4d37e24e7d0860b70461564e6dc84b32b88e07
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-06-08 11:19:42 +0530

    Time: 2 ms (65.66%), Space: 41.5 MB (43.31%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,15 +1,13 @@
 class Solution {
     public int reverse(int x) {
         int rev = 0;
-        int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;
         
         while(x != 0){
             int lastDigit = x % 10;
             x = x / 10;
-            if(rev > max/10 || (rev == max / 10 && lastDigit > 7))
+            if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && lastDigit > 7))
                 return 0;
-            if(rev < min/10 || (rev == min / 10 && lastDigit < -8))
+            if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && lastDigit < -8))
                 return 0;
             rev = rev * 10 + lastDigit;
         }
