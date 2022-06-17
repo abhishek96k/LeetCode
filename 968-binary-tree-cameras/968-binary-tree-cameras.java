81ac0a58766f6336cee7f70dca9ac5daa2bd9f52
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-06-17 08:12:06 +0530

    Time: 1 ms (76.65%), Space: 44.1 MB (14.09%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minCameraCover(TreeNode root) {
+        int[] ans = solve(root);
+        return Math.min(ans[1], ans[2]);
+    }
+    
+    public int[] solve(TreeNode node) {
+        if(node == null) 
+            return new int[] {0, 0, 99999};
+        
+        int[] L = solve(node.left);
+        int[] R = solve(node.right);
+        int mL12 = Math.min(L[1], L[2]);
+        int mR12 = Math.min(R[1], R[2]);
+        
+        int d0 = L[1] + R[1];
+        int d1 = Math.min(L[2] + mR12, R[2] + mL12);
+        int d2 = 1 + Math.min(L[0], mL12) + Math.min(R[0], mR12);
+        return new int[]{d0, d1, d2};
+    }
+}
