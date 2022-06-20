1c1b924dd38863a65167060862dfdd93d9574c59
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-06-20 12:00:36 +0530

    Time: 1 ms (100.00%), Space: 43.8 MB (95.61%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -3,21 +3,21 @@ class Solution {
         int size = nums.length;
         int p1 = 0, p2 = nums.length - 1;
         int curr = size - 1;
-        int[] output = new int[size];
+        int[] ans = new int[size];
         
         while(p1 <= p2) {
             int tempLeft = nums[p1] * nums[p1];
             int tempRight = nums[p2] * nums[p2];
             if(tempLeft > tempRight) {
-                output[curr] = tempLeft;
+                ans[curr] = tempLeft;
                 curr--;
                 p1++;
             } else {
-                output[curr] = tempRight;
+                ans[curr] = tempRight;
                 curr--;
                 p2--;
             }
         }
-        return output;
+        return ans;
     }
 }
