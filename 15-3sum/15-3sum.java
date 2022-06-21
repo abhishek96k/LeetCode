d77c4d58d0b116d2302ca559a60deb60fa708495
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-06-21 17:21:03 +0530

    Time: 27 ms (78.84%), Space: 59 MB (50.55%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,27 +1,33 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        
-        List<List<Integer>> result = new ArrayList<>();
-        for(int num1Idx = 0; num1Idx < nums.length; num1Idx++){
-            if(num1Idx > 0 && nums[num1Idx] == nums[num1Idx - 1]){
-                continue;
-        }
-            int num2Idx = num1Idx + 1;
-            int num3Idx = nums.length - 1;
-            while(num2Idx < num3Idx){
-                int sum = nums[num2Idx] + nums[num3Idx] + nums[num1Idx];
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[num1Idx], nums[num2Idx], nums[num3Idx]));
-                    num3Idx--;
-                    while(num2Idx < num3Idx && nums[num3Idx] == nums[num3Idx + 1]) num3Idx--;
-                } else if(sum > 0){
-                    num3Idx--;
-                } else {
-                    num2Idx++;
-                }
-            }
-        }
-        return result;
+    public static List<List<Integer>> threeSum(int[] nums) {
+       List<List<Integer>> result = new ArrayList<List<Integer>>();
+      if(nums == null || nums.length < 3)
+          return result;
+      // sort the array in-place
+      Arrays.sort(nums);
+      for (int i = 0; i < nums.length - 2; i++) {
+          if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+              int low = i+1, high = nums.length - 1;
+              int sum = 0 - nums[i];
+                // Finding two sum using two pointer
+              while (low < high) {
+                 if (nums[low] + nums[high] == sum) {
+                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                      // if next number is duplicates skip that
+                    while (low < high && nums[low] == nums[low+1]) low++;
+                      // if previous number is duplicates skip that
+                    while (low < high && nums[high] == nums[high-1]) high--;
+                    low++; 
+                  high--;
+                 } else if (nums[low] + nums[high] < sum) {
+                    low++;
+                 } else {
+                    high--;
+                 }
+              }
+          }
+      }
+      
+       return result;
     }
 }
