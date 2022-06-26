10d8deb5c598d55067b19cb5981dd91bb1157d13
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-06-26 12:26:31 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (84.36%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,13 +1,25 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i = 0; i < (n + 1)/2; i++){
-            for(int j = 0; j < n / 2; j++){
-                int temp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
+        mirrorSecondaryDiagonal(matrix);
+        mirrorHorizontally(matrix);
+    }
+    
+    private static void mirrorSecondaryDiagonal(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length - i - 1; j++) {
+                int curr = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - j - 1][matrix.length - i - 1];
+                matrix[matrix.length - j -1][matrix.length - i - 1] = curr;
+            }
+        }
+    }
+    
+    private static void mirrorHorizontally(int[][] matrix) {
+        for(int i = 0; i < matrix.length / 2; i++) {
+            for(int j = 0; j < matrix.length; j++) {
+                int curr = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - i -1][j];
+                matrix[matrix.length - i - 1][j] = curr;
             }
         }
     }
