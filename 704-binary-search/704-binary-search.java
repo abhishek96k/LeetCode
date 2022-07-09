2124e2ef7fcead012ae7ea486f50e9f777014322
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-09 23:45:30 +0530

    Time: 1 ms (15.75%), Space: 54 MB (65.38%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -5,16 +5,16 @@ class Solution {
         
         while(left <= right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] < target){
+            if(nums[mid] < target) {
                 left = mid + 1;
             }
-            if(nums[mid] > target){
+            if(nums[mid] > target) {
                 right = mid - 1;
             }
-            if(nums[mid] == target){
+            if(nums[mid] == target) {
                 return mid;
             }
         }
-        return - 1;
+        return -1;
     }
 }
