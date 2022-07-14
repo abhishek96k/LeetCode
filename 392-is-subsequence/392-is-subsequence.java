354e389a38213ef63930874a14fcc821cd68cbd8
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-14 14:26:47 +0530

    Time: 2 ms (46.82%), Space: 41.9 MB (58.45%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -5,22 +5,14 @@ class Solution {
         int p1 = 0, p2 = 0;
         
         while(p1 < s.length() && p2 < t.length()) {
-            
             if(s.charAt(p1) == t.charAt(p2)) {
                 p1++;
                 p2++;
-            } 
-            else {
+            } else {
                 p2++;
             }
-            
-            if(p1 == s.length()) {
-                return true;
-            }
-            
+            if(p1 == s.length()) return true;
         }
-        
         return false;
     }
 }
-// TC: o(max(m,n)) SC: o(1)
