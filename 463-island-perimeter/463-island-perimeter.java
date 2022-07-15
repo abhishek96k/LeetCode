cd9dbba45e023f2547fed6831575c2d46e3e3550
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-15 12:42:20 +0530

    Time: 9 ms (81.80%), Space: 43.4 MB (85.28%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -3,7 +3,7 @@ class Solution {
     public int islandPerimeter(int[][] grid) {
         count = 0;
         for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[i].length; j++){
+            for(int j = 0; j < grid[i].length; j++) {
                 if(grid[i][j] == 1) {
                     dfs(grid,i,j);
                     break;
@@ -13,7 +13,7 @@ class Solution {
         return count;
     }
     
-    public void dfs(int[][] grid, int i, int j) {
+    public void dfs(int[][] grid, int i, int j){
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
             count++;
             return;
@@ -22,9 +22,9 @@ class Solution {
         
         grid[i][j] = -1;
         
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
 }
