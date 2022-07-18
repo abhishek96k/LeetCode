979bbf367c207faa1f77829847bc3de4e8cfe7fe
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-18 13:30:58 +0530

    Time: 2 ms (78.80%), Space: 52.2 MB (64.83%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -4,7 +4,7 @@ class Solution {
         int leftSum = 0;
         
         for(int i : nums) sum += i;
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++){
             if(leftSum == sum - leftSum - nums[i]) return i;
             leftSum += nums[i];
         }
