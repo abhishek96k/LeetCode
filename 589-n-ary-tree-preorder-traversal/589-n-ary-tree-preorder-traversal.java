b4c46dd8c16d25b819b51f241c6fc3c396aa300c
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-18 10:28:55 +0530

    Time: 1 ms (90.36%), Space: 46.6 MB (14.46%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -1,16 +1,15 @@
 
 class Solution {
-    List<Integer> ans = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
     public List<Integer> preorder(Node root) {
-    
-        if(root == null) return ans;
+        if(root == null) return list;
         preOrderHelper(root);
-        return ans;
+        return list;
     }
     
-    public void preOrderHelper(Node node){
+    public void preOrderHelper(Node node) {
         if(node.children == null) return;
-        ans.add(node.val);
+        list.add(node.val);
         for(Node child : node.children){
             preOrderHelper(child);
         }
