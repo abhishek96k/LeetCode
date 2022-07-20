8c0ebfff549a26bfa5feddb428eb2a94d846f694
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-20 14:03:50 +0530

    Time: 1 ms (93.01%), Space: 42.2 MB (32.57%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -4,8 +4,8 @@ class Solution {
         
         int p1 = 0, p2 = 0;
         
-        while(p1 < s.length() && p2 < t.length()) {
-            if(s.charAt(p1) == t.charAt(p2)) {
+        while(p1 < s.length() && p2 < t.length()){
+            if(s.charAt(p1) == t.charAt(p2)){
                 p1++;
                 p2++;
             } else {
