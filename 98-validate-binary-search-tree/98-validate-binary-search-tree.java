99376ce7acd3a29e05e36bec2c155a57504a4586
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-20 15:04:47 +0530

    Time: 3 ms (14.63%), Space: 44.2 MB (34.89%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left; 
+            }
+            root = stack.pop();
+            if(pre != null && pre.val >= root.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
