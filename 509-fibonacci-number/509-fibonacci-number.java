6047cb2ce9bacd3541cbbb26220633e25d629903
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-07-22 19:41:34 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (70.42%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -6,11 +6,12 @@ class Solution {
         if(n <= 1) {
             return n;
         }
-        if(dp[n] > 0) {
+        
+        if(dp[n] > 0){
             return dp[n];
         }
         
-        dp[n] = fib(n - 1) + fib(n - 2);
+        dp[n] = fib(n-1)+fib(n-2);
         return dp[n];
     }
 }
