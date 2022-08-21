d5ecb75e2efbb0b03ec7ef5ba64d74772fcaa285
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-08-21 19:00:04 +0530

    Time: 2 ms (83.17%), Space: 54.5 MB (68.32%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,7 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         if(nums.length == 1) return;
-        
         int left = 0;
         for(int right = 0; right < nums.length; right++){
             if(nums[right] != 0){
@@ -9,10 +8,8 @@ class Solution {
                 left++;
             }
         }
-//         form index : left to nums.length - 1, with 0's
         for(int i = left; i < nums.length; i++){
             nums[i] = 0;
         }
     }
 }
-// TC: o(n) SC: o(1)
