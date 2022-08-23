cb330cff742fb9443c4d853106b65865215a6682
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-08-23 10:55:56 +0530

    Time: 4 ms (97.84%), Space: 96.6 MB (81.03%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -1,34 +1,27 @@
-// TC: O(N) SC: O(1)
-public class Solution {
+
+class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null) {
-            return true;
-        }
+        if(head == null || head.next == null) return true;
         
-        // find middle of the given linked list && p1 will point to middle node
         ListNode p1 = head;
         ListNode p2 = head;
-        while (p2 != null && p2.next != null) {
+        while(p2 != null && p2.next != null){
             p1 = p1.next;
             p2 = p2.next.next;
         }
         
-        // reversing first half
-        ListNode tmp = null;
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != p1) {
-            tmp = curr.next;
+        ListNode temp = null, prev = null, curr = head;
+        while(curr != p1){
+            temp = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = tmp;
+            curr = temp; 
         }
-
-        // odd number of elements, need left move p1 one step
-        if (p2 != null && p2.next == null) p1 = p1.next;
         
-        while(p1 != null) {
-            if (p1.val != prev.val) return false;
+        if(p2 != null && p2.next == null) p1 = p1.next;
+        
+        while(p1 != null){
+            if(p1.val != prev.val) return false;
             p1 = p1.next;
             prev = prev.next;
         }
