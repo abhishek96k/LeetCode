1e2e8381baae73a7fb52bed9d06ec5308745a1f3
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-08-24 18:28:07 +0530

    Time: 1 ms (81.40%), Space: 42.7 MB (50.20%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        
         int[] orderMap = new int[26];
         
         for(int i = 0; i < order.length(); i++){
@@ -9,15 +8,16 @@ class Solution {
         
         for(int i = 0; i < words.length - 1; i++){
             for(int j = 0; j < words[i].length(); j++){
+                
                 if(j >= words[i + 1].length()) return false;
                 if(words[i].charAt(j) != words[i + 1].charAt(j)){
                     int currentWordChar = words[i].charAt(j) - 'a';
                     int nextWordChar = words[i + 1].charAt(j) - 'a';
-                    if(orderMap[currentWordChar] > orderMap[nextWordChar])
-                        return false;
                     
+                    if(orderMap[currentWordChar] > orderMap[nextWordChar]) return false;
                     else break;
-                }
+                    
+                 }
             }
         }
         return true;
