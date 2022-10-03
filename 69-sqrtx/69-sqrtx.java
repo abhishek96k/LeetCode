50eb47ad0630dc1af69528e86ce33638e633060c
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-10-03 17:32:29 +0530

    Time: 2 ms (84.73%), Space: 41.3 MB (51.02%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,5 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if(x <= 1) return x;
+        int start = 1;
+        int end = x/2;
+        
+        while(start < end) {
+            int mid = (start + (end-start)/2) + 1;
+            
+            // use division instead of multiplication to avoid overflow
+            int div = x/mid;
+            if(div == mid) return mid;
+            if(div > mid) start = mid;
+            else end = mid-1;
+        }
+        
+        return start;
     }
 }
