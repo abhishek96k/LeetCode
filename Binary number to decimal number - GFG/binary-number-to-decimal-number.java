c2129a9dc115da519e2d5c2bf4cb97fb580f32e6
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2022-11-23 21:21:12 +0530

    Added solution - LeetHub

---
+++ Binary number to decimal number - GFG/binary-number-to-decimal-number.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.binary_to_decimal(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int binary_to_decimal(String str) {
+        int m = 0;
+        int n = 1;
+        for(int i = str.length() - 1; i >= 0; i--){
+            m += (str.charAt(i) - '0') * n;
+            n = n * 2;
+        }
+        return m;
+    }
+}
