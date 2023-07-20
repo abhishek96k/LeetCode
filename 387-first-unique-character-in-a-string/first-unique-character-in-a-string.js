ee90a9a4f2881a5f977f5aa3e7c75f0b83539e45
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-07-20 21:44:28 +0530

    Time: 94 ms (81.62%) | Memory: 44.6 MB (92.56%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -3,22 +3,10 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    // Create a hash table to store the characters and their count. 
-    const hashTable = {};
-
-    // Iterate through the entire string to store the characters and their counts in the hash table.
-    for(let char of s) {
-        if(hashTable[char])
-            hashTable[char]++;
-        else
-            hashTable[char] = 1;
+    for(let i = 0; i < s.length; i++) {
+        const ch = s[i];
+        if(s.indexOf(ch) === s.lastIndexOf(ch))
+            return i;
     }
-// Check if there is any unique character and return its index.
-for(let i = 0; i < s.length; i++) {
-    const char = s[i];
-    if(hashTable[char] == 1)
-        return i;
-}
-// If there is no unique character, return -1.
-return -1;
+    return -1;
 };
