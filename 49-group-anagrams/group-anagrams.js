9c4a3a33d397f79cc59653283fb86d4b70491140
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-07-20 23:25:27 +0530

    Time: 125 ms (73.80%) | Memory: 53.7 MB (53.29%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -6,8 +6,10 @@ var groupAnagrams = function(strs) {
     const ht = {};
     for(let str of strs) {
         const sorted = str.split('').sort().join('');
-        if (ht[sorted]) ht[sorted].push(str);
-        else ht[sorted] = [str];
+        if(ht[sorted])
+            ht[sorted].push(str);
+        else
+            ht[sorted] = [str];
     }
     return Object.values(ht);
 };
