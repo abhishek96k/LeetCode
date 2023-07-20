1129485f15325fed2b3c85b95707a5699db9090e
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-07-20 21:24:39 +0530

    Time: 115 ms (38.50%) | Memory: 44.9 MB (82.13%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -4,12 +4,13 @@
  */
 var firstUniqChar = function(s) {
     const hashTable = {};
-    for(let char of s) {
+    for(let char of s){
         if(hashTable[char])
             hashTable[char]++;
         else
             hashTable[char] = 1;
     }
+
     for(let i = 0; i < s.length; i++) {
         const char = s[i];
         if(hashTable[char] == 1) return i;
