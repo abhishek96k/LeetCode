bbf3ee16ea9b39e13fe5808999614cf4ddcfcb2f
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-07-26 13:20:29 +0530

    Time: 68 ms (53.97%) | Memory: 44.2 MB (90.93%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let dummy = init;
+
+    function increment() {
+        init++;
+        return init; 
+    }
+
+    function decrement() {
+        init--;
+        return init; 
+    }
+
+    function reset() {
+        init = dummy;
+        return init; 
+    }
+
+    return {increment, decrement, reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
