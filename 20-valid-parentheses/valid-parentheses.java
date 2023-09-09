5447d411b18748a97c9d68f688e46380486b726e
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-09-09 15:56:59 +0530

    Time: 2 ms (84.57%) | Memory: 40.9 MB (15.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,7 +5,7 @@ class Solution {
             if(ch == '(') stack.push(')');
             else if(ch == '[') stack.push(']');
             else if(ch == '{') stack.push('}');
-            else
+            else 
                 if(stack.isEmpty() || stack.pop() != ch)
                     return false;
         }
