ce01acef2f8e1c9053446feb7d2f96d05dcc6fee
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-09-10 15:54:53 +0530

    Time: 10 ms (86.60%) | Memory: 54.8 MB (85.27%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] == nums[i + 1]) return true;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num))
+                return true;
+            set.add(num);
         }
         return false;
     }
