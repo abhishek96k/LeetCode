c5af6ec7203a3d4838825c45af2cac0043b790dc
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-09-11 10:39:08 +0530

    Time: 13 ms (33.26%) | Memory: 43.7 MB (29.73%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        for(int val : map.values()) {
+            if(val != 0) return false;
+        }
+        return true;
+    }
+}
