550e5246220dc7b53243832570c70b7ff3905a95
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2023-09-16 20:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (6.45%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,8 +11,8 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
         ListNode fast = head;
+        ListNode slow = head;
         while(fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
