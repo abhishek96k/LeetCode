a040fdd7479b789e874c66f2affc6bc9f13b5bff
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-02-03 19:54:27 +0530

    Time: 53 ms (75.72%) | Memory: 51 MB (10.22%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.ts
@@ -0,0 +1,19 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let flag: boolean = false;
+    return function (...args) {
+        if(flag) return undefined;
+        flag = true;
+        return fn(...args);
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
