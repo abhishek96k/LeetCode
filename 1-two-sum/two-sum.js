f4057a714316e494b56e9137d1102895fe7ef3b0
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-02-23 23:34:36 +0530

    Time: 61 ms (68.66%) | Memory: 51.3 MB (16.83%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,11 +3,11 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     const ans = {};
-    for(let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const want = target - num;
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        let want = target - num;
         if (want in ans) {
             const left = ans[want];
             return [left, i];
