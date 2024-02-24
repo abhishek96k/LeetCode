98e5b649e5e8456635ce7090c9aa54a611687f51
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-02-24 14:18:01 +0530

    Time: 70 ms (30.11%) | Memory: 50.7 MB (33.97%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) return false;
+    const sHash = {};
+    const tHash = {};
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+        if (!sHash[charS]) {
+            sHash[charS] = charT;
+        }
+        if (!tHash[charT]) {
+            tHash[charT] = charS;
+        }
+        if (sHash[charS] !== charT || tHash[charT] !== charS) {
+            return false;
+        }
+    }
+    return true;
+};
