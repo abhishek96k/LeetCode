e80a447b4778ef6a5e9386670eb4b2fd45940349
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-06-07 11:41:39 +0530

    Time: 12 ms (34.84%) | Memory: 44.4 MB (39.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -10,10 +10,10 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0) - 1);
         }
 
-        for (int val : map.values())
-            if (val != 0) {
+        for (int val : map.values()) {
+            if (val != 0)
                 return false;
-            }
+        }
         return true;
     }
 }
