f376f39152fcf3202403529bd6cfd20cf48082c3
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-06-16 22:51:50 +0530

    Time: 18 ms (5.74%) | Memory: 44.8 MB (22.99%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count = 0;
+        boolean flag = true;
+        String ans = "";
+
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == '(')
+                count++;
+            else if(s.charAt(i) == ')')
+                count--;
+
+            if(count == 1 && flag == true){
+                flag = false;
+                continue;
+            }
+            
+            if(count == 0 && flag == false){
+                flag = true;
+                continue;
+            }
+            ans += s.charAt(i);
+        }
+        return ans;
+    }
+}
