7db82f2290024fb157201b4d5a43d57b2b2fbc3c
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-08-30 08:48:20 +0530

    Time: 73 ms (16.92%) | Memory: 51.7 MB (64.98%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -2,14 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var getConcatenation = function (nums) {
-    const ans = [];
-    let count = 2;
-    while (count !== 0) {
-        for (let i = 0; i < nums.length; i++) {
-            ans.push(nums[i]);
-        }
-        count--;
-    }
+var getConcatenation = function(nums) {
+    let ans = [...nums, ...nums];
     return ans;
 };
