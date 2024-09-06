63ada55bbb7b35d6d08c3359e2dbd006199e0314
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-09-06 11:02:33 +0530

    Time: 64 ms (5.78%) | Memory: 49.1 MB (13.35%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -3,12 +3,18 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-    const dp = new Array(n - 1).fill(0);
-    dp[0] = 1;
-    dp[1] = 1;
-
-    for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+    if (n <= 3) {
+        return n;
     }
-    return dp[n];
+
+    let prev1 = 3;
+    let prev2 = 2;
+    let curr = 0;
+
+    for (let i = 3; i < n; i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return curr;
 };
