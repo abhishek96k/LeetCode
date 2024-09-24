40227eedc426abc137d2182a0ba02160a9618876
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2024-09-24 09:41:28 +0530

    Time: 270 ms (32.19%) | Memory: 74.4 MB (31.44%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -5,29 +5,30 @@
 var sortArray = function (nums) {
     if (nums.length <= 1) return nums;
 
-    const midPoint = Math.floor(nums.length / 2);
-    const left = nums.slice(0, midPoint);
-    const right = nums.slice(midPoint);
+    let midPoint = Math.floor(nums.length / 2);
+    let left = nums.slice(0, midPoint);
+    let right = nums.slice(midPoint);
 
-    const sortedLeft = sortArray(left);
-    const sortedRight = sortArray(right);
+    let sortedLeft = sortArray(left);
+    let sortedRight = sortArray(right);
 
     return merge(sortedLeft, sortedRight);
 };
 
 function merge(left, right) {
-    const sortedResult = [];
+    const result = [];
     let l = 0, r = 0;
 
     while (l < left.length && r < right.length) {
         if (left[l] < right[r]) {
-            sortedResult.push(left[l]);
+            result.push(left[l]);
             l++;
         } else {
-            sortedResult.push(right[r]);
+            result.push(right[r]);
             r++;
         }
     }
-    sortedResult.push(...left.slice(l), ...right.slice(r));
-    return sortedResult;
+
+    result.push(...left.slice(l), ...right.slice(r));
+    return result;
 }
