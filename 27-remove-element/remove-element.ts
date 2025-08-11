5f1a2b156241f78462d3f67ad0b4c26f10ac93c6
Abhishek Nalawade <54280252+abhishek96k@users.noreply.github.com>
2025-08-11 18:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (30.03%) - LeetSync

---
+++ 27-remove-element/remove-element.ts
@@ -0,0 +1,10 @@
+function removeElement(nums: number[], val: number): number {
+    let x = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+    return x;
+};
